964382b1a16b657ba46e5fc411cbf80c1303efc6
Thomas Røkke <thomas.roekke@gmail.com>
2019-01-14 14:28:05 +0100

    Opprettet testingområde for resources/componentes/layouts

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -80,8 +80,31 @@
             @endif
 
             <div class="content">
-                <div class="title m-b-md">
-                    Vælkømmin tel gars. Jøss. Klarte å start en lokal server jo. Itte værst.
+
+                <div class="container">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="card text-center">
+                            <div class="card-header">
+                                <ul class="nav nav-pills card-header-pills">
+                                    <li class="nav-item">
+                                        <a class="nav-link active" href="#">Active</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#">Link</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
 
 
