12dba110f849a85fe426c474eb0c3e8910e820b6
Thomas Røkke <thomas.roekke@gmail.com>
2019-01-16 19:30:25 +0100

    fix

--- app/resources/views/proto.blade.php
+++ app/resources/views/proto.blade.php
@@ -8,6 +8,10 @@
 
 @section('content')
     <div class="container">
+        <a class="weatherwidget-io" href="https://forecast7.com/no/59d9110d75/oslo/" data-label_1="OSLO" data-label_2="VÆRMELDING" data-theme="original" >OSLO VÆRMELDING</a>
+        <script>
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
+        </script>
 
         <div class="jumbotron" style="background-color:white">
             <h1 class="display-6">God dag, Thomas</h1>
@@ -16,10 +20,7 @@
             <a class="btn btn-primary btn-lg" href="{{ route('drive') }}" role="button">Se kjøreliste</a>
         </div>
 
-        <a class="weatherwidget-io" href="https://forecast7.com/no/59d9110d75/oslo/" data-label_1="OSLO" data-label_2="VÆRMELDING" data-theme="original" >OSLO VÆRMELDING</a>
-        <script>
-            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
-        </script>
+
 
         <a class="twitter-timeline" href="https://twitter.com/NRKTrafikkOslo?ref_src=twsrc%5Etfw">Tweets by NRKTrafikkOslo</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
