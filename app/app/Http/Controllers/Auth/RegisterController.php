34fc9cf4bd56a7d357739b4cfdb25b56b30ec474
Thomas Røkke <thomas.roekke@gmail.com>
2019-01-28 13:10:48 +0100

    Added default level 1 (user) role to newly registered users under RegisterController.php. Other user types must be explicit defined

--- app/app/Http/Controllers/Auth/RegisterController.php
+++ app/app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,8 @@ use App\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use jeremykenedy\LaravelRoles\Models\Role;
+use jeremykenedy\LaravelRoles\Models\Permission;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
@@ -63,10 +65,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
-            'password' => Hash::make($data['password']),
+            'password' => bcrypt($data['password']),
         ]);
+
+        $role = Role::where('name', '=', 'User')->first();  //choose the default role upon user creation.
+        $user->attachRole($role);
+
+        return $user;
+
     }
+
 }
