c998ef9f37e7682e935b55e6436a909c9ad3e673
Thomas Røkke <thomas.roekke@gmail.com>
2019-01-30 09:39:40 +0100

    transport.home route removed from views

--- app/resources/views/components/_auth-nav.blade.php
+++ app/resources/views/components/_auth-nav.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
 
 
-        <a class="navbar-brand meko-color-text" href="{{ route('transport.home') }}">Mekodrive</a>
+        <a class="navbar-brand meko-color-text" href="{{ route('home') }}">Mekodrive</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
