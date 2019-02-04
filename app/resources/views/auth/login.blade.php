117842bc091bb5fdaf369698e939ffbfccc1dcbb
Simen <johsim16@student.westerdals.no>
2019-02-04 10:05:35 +0100

    Fiksa litt på login

--- app/resources/views/auth/login.blade.php
+++ app/resources/views/auth/login.blade.php
@@ -7,7 +7,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header meko-default">{{ __('Login') }}</div>
+                <div class="card-header meko-default">{{ __('Logg inn') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -56,7 +56,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-outline-meko">
-                                    {{ __('Login') }}
+                                    {{ __('Logg inn') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
