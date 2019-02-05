b01f231d262c1c55817d52fe3d903d6948dafb81
Thomas Røkke <thomas.roekke@gmail.com>
2019-02-05 18:13:03 +0100

    removed embeded tableau

--- app/resources/views/office/home.blade.php
+++ app/resources/views/office/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12">
-            <script type='text/javascript' src='https://eu-west-1a.online.tableau.com/javascripts/api/viz_v1.js'></script><div class='tableauPlaceholder' style='width: 1000px; height: 827px;'><object class='tableauViz' width='1000' height='827' style='display:none;'><param name='host_url' value='https%3A%2F%2Feu-west-1a.online.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='&#47;t&#47;tullbal' /><param name='name' value='Book1&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='showAppBanner' value='false' /><param name='filter' value='iframeSizedToWindow=true' /></object></div>
+        <h1 class="text-center">Home</h1>
         </div>
     </div>
 </div>
--- app/resources/views/office/routes.blade.php
+++ app/resources/views/office/routes.blade.php
@@ -81,68 +81,100 @@
 
 
 
-                <table class="table">
-                    <thead class="meko-default">
-                    <tr>
-                        <th scope="col">Dato</th>
-                        <th scope="col">Rute</th>
-                        <th scope="col">Tid</th>
-
-                        <th scope="col">Sjåfør</th>
-                        <th scope="col">Aktiv status</th>
-                        <th scope="col">Optimer </th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($routes as $w)
-
+                    <table class="table">
+                        <thead class="meko-default">
                         <tr>
-                            <th scope="row">{{ $w->date }}</th>
-                            <td>{{ $w->route }}</td>
-                            <td>{{ $w->time }}</td>
+                            <th scope="col">Dato</th>
+                            <th scope="col">Rute</th>
+                            <th scope="col">Tid</th>
 
-
-                            <td><div class="dropdown">
-                                    <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                        {{ (!empty($w->driver)) ? $w->driver->name : 'Ikke valgt' }}
-                                    </button>
-                                    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                                        @foreach($users as $user)
-                                            <a href="{{ route('setdriver', ['route_id' => $w->id, 'driver_id' => $user->id]) }}" class="dropdown-item">{{ $user->name }}</a>
-                                        @endforeach
-                                    </div>
-                                </div>
-                            </td>
-
-                            <td>
-                                @if($w->active === 1)
-                                    <a href="{{ route('setinactive', ['route_id' => $w->id]) }}" class="btn btn-success">JA</a>
-
-                                @else
-                                    <a href="{{ route('setactive', ['route_id' => $w->id]) }}" class="btn btn-warning">NEI</a>
-                                @endif
-                            </td>
-                            <td> <a href="{{ route('optimize', ['route_id' => $w->id]) }}" class="btn btn-warning">Optimer</a></td>
-
-
+                            <th scope="col">Sjåfør</th>
+                            <th scope="col">Aktiv status</th>
+                            <th scope="col">Optimer </th>
                         </tr>
+                        </thead>
+                        <tbody>
+                        @foreach($routes as $w)
 
-                        @foreach($w->stops as $s)
                             <tr>
-                                <th scope="row">{{ $s->workshop->name }}</th>
-                                <td>{{ $s->workshop_id }}</td>
-                                <td>{{ $s->route_position }}</td>
+                                <th scope="row">{{ $w->date }}</th>
+                                <td>{{ $w->route }}</td>
+                                <td>{{ $w->time }}</td>
+
+
+                                <td><div class="dropdown">
+                                        <button class="btn dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                            {{ (!empty($w->driver)) ? $w->driver->name : 'Ikke valgt' }}
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                            @foreach($users as $user)
+                                                <a href="{{ route('setdriver', ['route_id' => $w->id, 'driver_id' => $user->id]) }}" class="dropdown-item">{{ $user->name }}</a>
+                                            @endforeach
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td>
+                                    @if($w->active === 1)
+                                        <a href="{{ route('setinactive', ['route_id' => $w->id]) }}" class="btn btn-success">JA</a>
+
+                                    @else
+                                        <a href="{{ route('setactive', ['route_id' => $w->id]) }}" class="btn btn-warning">NEI</a>
+                                    @endif
+                                </td>
+                                <td> <a href="{{ route('optimize', ['route_id' => $w->id]) }}" class="btn btn-warning">Optimer</a></td>
+
 
                             </tr>
+
+
+                            @foreach($w->stops as $s)
+                                <tr data-toggle="collapse" href="#collapseExample{{ $s->id }}" role="button" aria-expanded="false" aria-controls="collapseExample{{ $s->id }}">
+                                    <th scope="row">{{ $s->workshop->name }}</th>
+                                    <td>{{ $s->workshop_id }}</td>
+                                    <td>{{ $s->route_position }}</td>
+
+
+                                </tr>
+
+
+
+
+                                  <tr>
+                                      <th scope="row" style="    border-top: none!important;">
+                                          <div class="collapse" id="collapseExample{{ $s->id }}">
+                                              <ul class="list-group">
+                                                  @foreach($s->orders as $o)
+                                                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                                                      {{ $o->ordernumber }}
+                                                      @if($o->delivered === 1)<span class="badge badge-success badge-pill"> <i class="fa fa-check-square text-white"></i>  </span>@else
+                                                          <span class="badge badge-primary badge-pill"><i class="fa fa-ban text-warning"></i></span>
+                                                      @endif
+
+                                                  </li>
+                                                  @endforeach
+
+                                              </ul>
+                                          </div>
+                                      </th>
+
+                                 </tr>
+
+                            @endforeach
+
+
+
                         @endforeach
 
 
+                        </tbody>
+                    </table>
 
-                    @endforeach
 
 
-                    </tbody>
-                </table>
+
+
+
             </div>
         </div><!-- end row -->
     </div><!-- end container-->
