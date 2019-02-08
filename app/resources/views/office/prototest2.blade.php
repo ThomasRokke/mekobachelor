690426ca0d706d34b15fc817f4c8ab81e7e686a5
Kristoffer <fylkri16@student.westerdals.no>
2019-02-08 12:45:20 +0100

    Added form to modal for making changes to orders

--- app/resources/views/office/prototest2.blade.php
+++ app/resources/views/office/prototest2.blade.php
@@ -1144,8 +1144,72 @@
                 <img src="https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX22409286.jpg">
             </div>
             <div class="description">
-                <div class="ui header">Her ytte ordre osv</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus magnam quo dolor illum molestias. Odio.</p>
+                <div class="ui header">Rediger ordre</div>
+
+                <!-- FORM THING -->
+               
+
+                                <div class="field">
+                                    <input type="text" name="card[expire-year]" maxlength="4" placeholder="Year">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <h4 class="ui dividing header">Receipt</h4>
+                    <div class="field">
+                        <label>Send Receipt To:</label>
+                        <div class="ui fluid multiple search selection dropdown">
+                            <input type="hidden" name="receipt">
+                            <i class="dropdown icon"></i>
+                            <div class="default text">Saved Contacts</div>
+                            <div class="menu">
+                                <div class="item" data-value="jenny" data-text="Jenny">
+                                    <img class="ui mini avatar image" src="/images/avatar/small/jenny.jpg">
+                                    Jenny Hess
+                                </div>
+                                <div class="item" data-value="elliot" data-text="Elliot">
+                                    <img class="ui mini avatar image" src="/images/avatar/small/elliot.jpg">
+                                    Elliot Fu
+                                </div>
+                                <div class="item" data-value="stevie" data-text="Stevie">
+                                    <img class="ui mini avatar image" src="/images/avatar/small/stevie.jpg">
+                                    Stevie Feliciano
+                                </div>
+                                <div class="item" data-value="christian" data-text="Christian">
+                                    <img class="ui mini avatar image" src="/images/avatar/small/christian.jpg">
+                                    Christian
+                                </div>
+                                <div class="item" data-value="matt" data-text="Matt">
+                                    <img class="ui mini avatar image" src="/images/avatar/small/matt.jpg">
+                                    Matt
+                                </div>
+                                <div class="item" data-value="justen" data-text="Justen">
+                                    <img class="ui mini avatar image" src="/images/avatar/small/justen.jpg">
+                                    Justen Kitsune
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ui segment">
+                        <div class="field">
+                            <div class="ui toggle checkbox">
+                                <input type="checkbox" name="gift" tabindex="0" class="hidden">
+                                <label>Do not include a receipt in the package</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ui button" tabindex="0">Submit Order</div>
+                </form>
+
+
+                <select class="ui dropdown">
+                    <option value="">RUTE</option>
+                    <option value="3">13</option>
+                    <option value="2">12</option>
+                    <option value="1">11</option>
+                    <option value="0">10</option>
+                </select>
+
             </div>
         </div>
         <div class="actions">
