c2076d20127306cce204b744d9a5fd5f79df2d24
Thomas Røkke <thomas.roekke@gmail.com>
2019-02-20 09:50:13 +0100

    route-preview view start

--- app/resources/views/transport/route-preview.blade.php
+++ app/resources/views/transport/route-preview.blade.php
@@ -74,93 +74,93 @@
 @endsection
 
 @section('scripts')
-    <script>
+<script>
 
 
 
-        // Initialize and add the map
-        function initMap() {
+    // Initialize and add the map
+    function initMap() {
 
-            // The map, centered at Uluru
-            var map = new google.maps.Map(
-                document.getElementById('map'));
-
-
-            bounds  = new google.maps.LatLngBounds();
+        // The map, centered at Uluru
+        var map = new google.maps.Map(
+            document.getElementById('map'));
 
 
-                    @foreach($route->stops as $stop)
-
-            var from{{ $stop->id }} = {lat:{!! $stop->workshop->lat !!}, lng:{!! $stop->workshop->lng !!}};
-
-            var content{{ $stop->id }} = '<div id="content">'+
-                '<div id="siteNotice">'+
-                '</div>'+
-                '<h5 id="firstHeading" class="firstHeading">{!! $stop->workshop->name !!}</h5>'+
-                '<div id="bodyContent">'+
-                '<h6><a target="_blank" href="https://www.google.com/maps?saddr=My+Location&daddr={!! $stop->workshop->adr !!}&destination_place_id={!! $stop->workshop->place_id !!}&travelmode=driving">' +
-                '                Veibeskrivelse <i class="fa fa-car"></i></a></h6>'+
-                '</div>'+
-                '</div>';
-
-            var infowindow{{ $stop->id }} = new google.maps.InfoWindow({
-                content: content{{ $stop->id }}
-            });
-
-            // The marker, positioned at Uluru
-            var marker{{ $stop->id }} = new google.maps.Marker({position: from{{ $stop->id }}, map: map, label:'{{ $stop->route_position }}'});
+        bounds  = new google.maps.LatLngBounds();
 
 
+                @foreach($route->stops as $stop)
 
-            loc{{ $stop->id }} = new google.maps.LatLng(marker{{ $stop->id }}.position.lat(), marker{{ $stop->id }}.position.lng());
+        var from{{ $stop->id }} = {lat:{!! $stop->workshop->lat !!}, lng:{!! $stop->workshop->lng !!}};
+
+        var content{{ $stop->id }} = '<div id="content">'+
+            '<div id="siteNotice">'+
+            '</div>'+
+            '<h5 id="firstHeading" class="firstHeading">{!! $stop->workshop->name !!}</h5>'+
+            '<div id="bodyContent">'+
+            '<h6><a target="_blank" href="https://www.google.com/maps?saddr=My+Location&daddr={!! $stop->workshop->adr !!}&destination_place_id={!! $stop->workshop->place_id !!}&travelmode=driving">' +
+            '                Veibeskrivelse <i class="fa fa-car"></i></a></h6>'+
+            '</div>'+
+            '</div>';
+
+        var infowindow{{ $stop->id }} = new google.maps.InfoWindow({
+            content: content{{ $stop->id }}
+        });
+
+        // The marker, positioned at Uluru
+        var marker{{ $stop->id }} = new google.maps.Marker({position: from{{ $stop->id }}, map: map, label:'{{ $stop->route_position }}'});
 
 
 
-            bounds.extend(loc{{ $stop->id }});
-
-
-            marker{{ $stop->id }}.addListener('click', function() {
-                infowindow{{ $stop->id }}.open(map, marker{{ $stop->id }});
-            });
-            @endforeach
-
-            map.fitBounds(bounds);
-            map.panToBounds(bounds);
-
-
-            var trafficLayer = new google.maps.TrafficLayer();
-            trafficLayer.setMap(map);
+        loc{{ $stop->id }} = new google.maps.LatLng(marker{{ $stop->id }}.position.lat(), marker{{ $stop->id }}.position.lng());
 
 
 
+        bounds.extend(loc{{ $stop->id }});
+
+
+        marker{{ $stop->id }}.addListener('click', function() {
+            infowindow{{ $stop->id }}.open(map, marker{{ $stop->id }});
+        });
+        @endforeach
+
+        map.fitBounds(bounds);
+        map.panToBounds(bounds);
+
+
+        var trafficLayer = new google.maps.TrafficLayer();
+        trafficLayer.setMap(map);
+
+
+
+    }
+
+
+
+
+</script>
+<!--Load the API from the specified URL
+* The async attribute allows the browser to render the page while the API loads
+* The key parameter will contain your own API key (which is not needed for this tutorial)
+* The callback parameter executes the initMap() function
+-->
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbs_N37A9PUe80-qtBc4EzC4_GJ_0PJKs&callback=initMap">
+</script>
+<script>
+
+
+
+    //check if jquery is loaded
+    window.onload = function() {
+        if (window.jQuery) {
+            // jQuery is loaded
+            //alert("Yeah!");
+        } else {
+            // jQuery is not loaded
+            alert("Jquery is not loaded");
         }
-
-
-
-
-    </script>
-    <!--Load the API from the specified URL
-    * The async attribute allows the browser to render the page while the API loads
-    * The key parameter will contain your own API key (which is not needed for this tutorial)
-    * The callback parameter executes the initMap() function
-    -->
-    <script async defer
-            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbs_N37A9PUe80-qtBc4EzC4_GJ_0PJKs&callback=initMap">
-    </script>
-    <script>
-
-
-
-        //check if jquery is loaded
-        window.onload = function() {
-            if (window.jQuery) {
-                // jQuery is loaded
-                //alert("Yeah!");
-            } else {
-                // jQuery is not loaded
-                alert("Jquery is not loaded");
-            }
-        }
-    </script>
+    }
+</script>
 
 @endsection
