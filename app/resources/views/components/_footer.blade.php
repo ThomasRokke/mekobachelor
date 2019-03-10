84f5054dae0a67c977152ea1a0ba6eb58bd3b6cc
Thomas Røkke <thomas.roekke@gmail.com>
2019-03-10 15:03:16 +0100

    Footer shrinked. Added YR weather info at driver homepage.

--- app/resources/views/components/_footer.blade.php
+++ app/resources/views/components/_footer.blade.php
@@ -1,34 +1,7 @@
-<footer style="margin-top:400px">
+<footer style="margin-top:920px">
     <div class="ui inverted vertical footer segment">
         <div class="ui center aligned container">
-            <div class="ui stackable inverted divided grid">
-                <div class="three wide column">
-                    <h4 class="ui inverted header">Lorem</h4>
-                    <div class="ui inverted link list">
-                        <a href="#" class="item">Link One</a>
-                        <a href="#" class="item">Link Two</a>
-                    </div>
-                </div>
-                <div class="three wide column">
-                    <h4 class="ui inverted header">Lorem</h4>
-                    <div class="ui inverted link list">
-                        <a href="#" class="item">Lorem</a>
-                        <a href="#" class="item">Lorem</a>
-                    </div>
-                </div>
-                <div class="three wide column">
-                    <h4 class="ui inverted header">Lorem</h4>
-                    <div class="ui inverted link list">
-                        <a href="#" class="item">Lorem</a>
-                        <a href="#" class="item">Lorem</a>
-                    </div>
-                </div>
-                <div class="seven wide column">
-                    <h4 class="ui inverted header">Om Mekodrive</h4>
-                    <p>Lorem ipsum dolor sit amet,<br> consectetur adipisicing elit. Natus, quia?</p>
-                </div>
-            </div>
-            <div class="ui inverted section divider"></div>
+
             <img src="https://www.mekonomen.no/Content/images/mekonomen-logo.svg" class="ui centered image">
             <div class="ui horizontal inverted small divided link list">
                 <a class="item" href="#">Sidekart</a>
@@ -37,7 +10,7 @@
             </div>
             <div class="ui inverted section">
                 <div class="ui horizontal inverted small divided link list">
-                    <p>Copyright or something?</p>
+                    <p>Mekonomen AS &copy; 2019</p>
                 </div>
             </div>
         </div>
